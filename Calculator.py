3a6da070bd00832d0535fef267c02c1fb63fb4e9
Navneet <navneet956044@gmail.com>
2025-10-04 06:23:07 +0530

    Add files via upload
    Here I used To Make Calculator using python

---
+++ Calculator.py
@@ -0,0 +1,13 @@
+a=float(input("Enter The Value :: "))
+c=(input("Enter The Operation :: "))
+b=float(input("Enter The Value :: "))
+if c=="+":
+    print("The Anwser is :: ",a+b)
+elif c=="-":
+    print("The Anwser is :: ",a-b)
+elif c=="*":
+    print("The Anwser is :: ",a*b)
+elif c=="/":
+    print("The Anwser is :: ",a/b)
+else :
+    print("Enter The Valid Operation :: ")
